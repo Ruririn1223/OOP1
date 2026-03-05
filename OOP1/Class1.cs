b1786c68245bc50d73777f60401461e420e2037c
Студент Студентов <is@athene.tech>
2026-03-05 09:45:30 +0300

    end-end

--- OOP1/Class1.cs
+++ OOP1/Class1.cs
@@ -2,27 +2,45 @@
 
 public class BankAccount
 {
-    private string _accountNumber { get; set; }
+    public string _accountNumber { get; set; }
     private string _ownerName { get; set; }
-	private decimal _balance { get; set; }
-	private decimal _interestRate { get; set; }
+    private double _balance { get; set; }
+	private double _interestRate { get; set; }
+    public string _discription { get; set; }
 
-	public BankAccount(string accountNumber, string ownerName, decimal initialBalance = 0, decimal interestRate = 0)
+	public BankAccount(string accountNumber, string ownerName, double initialBalance = 0, double interestRate = 0)
     {
         _accountNumber = accountNumber;
         _ownerName = ownerName;
         _balance = initialBalance >= 0 ? initialBalance : throw new ArgumentException("Начальный баланс не может быть отрицательным.");
         _interestRate = interestRate >= 0 ? interestRate : throw new ArgumentException("Процентная ставка не может быть отрицательной.");
     }
+    public BankAccount() {
+		_accountNumber = "RU00000000000";
+		_ownerName = "Неизвестно";
+        _balance = 1200;
+		_interestRate = 0.05;
+	}
 
-    public void Deposit(decimal amount)
+
+	public void Deposit( double amount)
     {
+
         if (amount <= 0)
             throw new ArgumentException("Сумма пополнения должна быть положительной.");
         _balance += amount;
     }
 
-    public void Withdraw(decimal amount)
+	public void Deposit1(int c, params double[] amount)
+	{
+        for (int i = 0; i < c; i++) {
+			if (amount[i] <= 0)
+				throw new ArgumentException("Сумма пополнения должна быть положительной.");
+			_balance += amount[i];
+		}
+	}
+
+	public void Withdraw(double amount)
     {
         if (amount <= 0)
             throw new ArgumentException("Сумма снятия должна быть положительной.");
@@ -40,6 +58,6 @@ public class BankAccount
     {
         Console.WriteLine($"Счёт: {_accountNumber}");
         Console.WriteLine($"Владелец: {_ownerName}");
-        Console.WriteLine($"Текущий баланс: {_balance:C}");
+        Console.WriteLine($"Текущий баланс: {_balance}");
     }
 }
