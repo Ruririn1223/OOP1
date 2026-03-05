b49079d90258b16a8cd304121af3ab6653e7fed2
Студент Студентов <is@athene.tech>
2026-03-05 10:32:00 +0300

    3

--- OOP1/BankAccaount.cs
+++ OOP1/BankAccaount.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 
-public class BankAccount
+internal abstract class BankAccount
+
 {
     public string _accountNumber { get; set; }
-    private string _ownerName { get; set; }
+    //private string _ownerName { get; set; }
     private double _balance { get; set; }
-	private double _interestRate { get; set; }
-    public string _discription { get; set; }
+	//private double _interestRate { get; set; }
+    //public string _discription { get; set; }
 
 	public BankAccount(string accountNumber, string ownerName, double initialBalance = 0, double interestRate = 0)
     {
         _accountNumber = accountNumber;
-        _ownerName = ownerName;
+        //_ownerName = ownerName;
         _balance = initialBalance >= 0 ? initialBalance : throw new ArgumentException("Начальный баланс не может быть отрицательным.");
-        _interestRate = interestRate >= 0 ? interestRate : throw new ArgumentException("Процентная ставка не может быть отрицательной.");
+        //_interestRate = interestRate >= 0 ? interestRate : throw new ArgumentException("Процентная ставка не может быть отрицательной.");
     }
     public BankAccount() {
 		_accountNumber = "RU00000000000";
-		_ownerName = "Неизвестно";
+		//_ownerName = "Неизвестно";
         _balance = 1200;
-		_interestRate = 0.05;
+		//_interestRate = 0.05;
 	}
 
 
@@ -49,15 +50,12 @@ public class BankAccount
         _balance -= amount;
     }
 
-    public void ApplyInterest()
-    {
-        _balance += _balance * _interestRate;
-    }
+    public abstract void CalculateInterest();
 
     public void PrintBalance()
     {
         Console.WriteLine($"Счёт: {_accountNumber}");
-        Console.WriteLine($"Владелец: {_ownerName}");
+        //Console.WriteLine($"Владелец: {_ownerName}");
         Console.WriteLine($"Текущий баланс: {_balance}");
     }
 }
---
+++ OOP1/SavingAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+	internal class SavingAccount: BankAccount
+	{
+		public SavingAccount(): base( string ownerName, double initialBalance = 0, double interestRate = 0) {
+		}
+	}
+}
